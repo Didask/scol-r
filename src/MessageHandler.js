e4de81d5bc6ee1d0def4d031e69ef03657e2484f
Unai <urainster@gmail.com>
2021-09-02 12:19:49 +0200

    Revert "Skip commit for all set value"
    This reverts commit f5734aadb374ddaf98e43d7aba4d3da90bafc6ee.

--- src/MessageHandler.js
+++ src/MessageHandler.js
@@ -20,26 +20,26 @@ function MessageHandler(win, sourceOrigin, adapter) {
   };
 
   this.setScore = function (score) {
-    adapter.setScore(score);
+    adapter.setScore(score, false);
   };
 
   this.setLessonStatus = function (lessonStatus) {
-    adapter.setLessonStatus(lessonStatus);
+    adapter.setLessonStatus(lessonStatus, false);
   };
 
   this.setObjectives = function (objectivesIds) {
-    adapter.setObjectives(objectivesIds);
+    adapter.setObjectives(objectivesIds, false);
   };
 
   this.setObjectiveScore = function (objectiveId, score) {
-    adapter.setObjectiveScore(objectiveId, score);
+    adapter.setObjectiveScore(objectiveId, score, false);
     var objectives = adapter.getObjectives();
     var completedObjectives = 0;
     for (var index = 0; index < objectives.length; index++) {
       if (adapter.getObjectiveScore(objectives[index])) completedObjectives++;
     }
     var globalScore = (completedObjectives / objectives.length) * 100;
-    adapter.setScore(globalScore);
-    if (globalScore >= 100) adapter.setLessonStatus("passed");
+    adapter.setScore(globalScore, false);
+    if (globalScore >= 100) adapter.setLessonStatus("passed", false);
   };
 }
